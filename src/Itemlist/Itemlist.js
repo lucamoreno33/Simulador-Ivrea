85b23e089fe3c14dfb2d062e26101c3a23b03c65
lucamoreno33 <luca_moreno2010@hotmail.com>
2024-06-06 13:46:52 -0300

    size and wrap

--- src/Itemlist/Itemlist.js
+++ src/Itemlist/Itemlist.js
@@ -3,7 +3,7 @@ import { Card } from "../Cards/Card";
 const Itemlist = ({ items }) => {
     console.log(items)
     return(
-        <div className="d-flex justify-content-center row">
+        <div className="flex-wrap d-flex justify-content-center row">
             { items.map((libro) => <Card key={libro.id} card={libro}/>) }
         </div>
     )
